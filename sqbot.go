eedf4a5f5673e2ca641146f3019fc88a1baf02dd
GOOLDOVE <81006632+GOOLDOVE@users.noreply.github.com>
2022-09-27 17:46:47 +0800

    Update sqbot.go

--- sqbot.go
+++ sqbot.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"time"
 
 	"github.com/RicheyJang/PaimengBot/manager"
 
@@ -34,24 +35,59 @@ var sendMsg message.Message
 // EchoHandler [5] Handler实现
 func sqbot(ctx *zero.Ctx) {
 	type Info struct {
-		All      int64  `json:"max_players"`
-		NOW      int64  `json:"currentPlayers"`
-		VISION   string `json:"version"`
-		MAP      string `json:"map"`
-		T1       string `json:"squad_teamOne"`
-		T2       string `json:"squad_teamTwo"`
-		TIME     string `json:"updatedAt"`
-		GAMEMODE string `json:"gameMode"`
-		NAME     string `json:"name"`
-		//time   string `json:"updatedAt"`
-		//time   string `json:"updatedAt"`
+		Data struct {
+			Type       string `json:"type"`
+			ID         string `json:"id"`
+			Attributes struct {
+				ID         string      `json:"id"`
+				Name       string      `json:"name"`
+				Address    interface{} `json:"address"`
+				IP         string      `json:"ip"`
+				Port       int         `json:"port"`
+				Players    int         `json:"players"`
+				MaxPlayers int         `json:"maxPlayers"`
+				Rank       int         `json:"rank"`
+				Location   []float64   `json:"location"`
+				Status     string      `json:"status"`
+				Details    struct {
+					Map                     string `json:"map"`
+					GameMode                string `json:"gameMode"`
+					Version                 string `json:"version"`
+					Secure                  int    `json:"secure"`
+					LicensedServer          bool   `json:"licensedServer"`
+					LicenseID               string `json:"licenseId"`
+					NumPubConn              int    `json:"numPubConn"`
+					NumPrivConn             int    `json:"numPrivConn"`
+					NumOpenPrivConn         int    `json:"numOpenPrivConn"`
+					SquadPlayerReserveCount int    `json:"squad_playerReserveCount"`
+					SquadPlayTime           int    `json:"squad_playTime"`
+					SquadPublicQueueLimit   int    `json:"squad_publicQueueLimit"`
+					SquadPublicQueue        int    `json:"squad_publicQueue"`
+					SquadReservedQueue      int    `json:"squad_reservedQueue"`
+					SquadTeamOne            string `json:"squad_teamOne"`
+					SquadTeamTwo            string `json:"squad_teamTwo"`
+					Modded                  bool   `json:"modded"`
+					ServerSteamID           string `json:"serverSteamId"`
+				} `json:"details"`
+				Private     bool      `json:"private"`
+				CreatedAt   time.Time `json:"createdAt"`
+				UpdatedAt   time.Time `json:"updatedAt"`
+				PortQuery   int       `json:"portQuery"`
+				Country     string    `json:"country"`
+				QueryStatus string    `json:"queryStatus"`
+			} `json:"attributes"`
+			Relationships struct {
+				Game struct {
+					Data struct {
+						Type string `json:"type"`
+						ID   string `json:"id"`
+					} `json:"data"`
+				} `json:"game"`
+			} `json:"relationships"`
+		} `json:"data"`
+		Included []interface{} `json:"included"`
 	}
 	jsonPath := "C:\\info.json"
-
-	fmt, err := os.Open("C:\\RCON\\app.js")
-	if err != nil {
-		panic(err)
-	}
 
 	jsonFile, err := os.Open(jsonPath)
 	if err != nil {
@@ -60,8 +96,6 @@ func sqbot(ctx *zero.Ctx) {
 	}
 	defer jsonFile.Close()
 
-	_ = fmt
-
 	var conf Info
 	decoder := json.NewDecoder(jsonFile)
 	err = decoder.Decode(&conf)
@@ -69,6 +103,6 @@ func sqbot(ctx *zero.Ctx) {
 		log.Printf("decode error [ %v ]", err)
 		return
 	} else {
-		ctx.Send(message.At(ctx.Event.UserID).String() + message.Text("服务器信息为：\n", "服务器名称：", conf.NAME, "\n", "当前人数：", conf.NOW, "/", conf.All, "\n", "服务器模式：", conf.GAMEMODE, "，", conf.T1, "vs", conf.T2, "\n", "服务器版本：", conf.VISION, "\n", "服务器地图：", conf.MAP).String() + sendMsg.String())
+		ctx.Send(message.At(ctx.Event.UserID).String() + message.Text("\n", "数据更新时间：(美国时间，中国加8小时）", conf.Data.Attributes.UpdatedAt, "\n", "服务器名称：", conf.Data.Attributes.Name, "\n", "当前人数：", conf.Data.Attributes.Players, "/", conf.Data.Attributes.MaxPlayers, "\n", "排队人数(不是实时)：", conf.Data.Attributes.Details.SquadPublicQueue, "\n", "服务器模式：", conf.Data.Attributes.Details.GameMode, "，", conf.Data.Attributes.Details.SquadTeamOne, "   VS   ", conf.Data.Attributes.Details.SquadTeamTwo, "\n", "服务器版本：", conf.Data.Attributes.Details.Version, "\n", "服务器地图：", conf.Data.Attributes.Details.Map).String() + sendMsg.String())
 	}
 }
